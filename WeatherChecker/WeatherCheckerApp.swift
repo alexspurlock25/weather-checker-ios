b05bea400361af937c1b39d63d7e719488d451a9
Alexander Spurlock <alexanderspurlock@gmail.com>
2023-05-17 12:49:23 -0400

    Initial Commit

---
+++ WeatherChecker/WeatherCheckerApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeatherCheckerApp.swift
+//  WeatherChecker
+//
+//  Created by Alexander Spurlock on 5/17/23.
+//
+
+import SwiftUI
+
+@main
+struct WeatherCheckerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
